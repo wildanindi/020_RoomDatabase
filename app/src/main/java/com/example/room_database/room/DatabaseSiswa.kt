a65112d484365e900bd696b03444cd5582ea3113
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-28 15:34:40 +0700

    Menambah companion object yang berisi 2 member class yaitu variabel Instance dan fungsi getDatabase

--- app/src/main/java/com/example/room_database/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/room_database/room/DatabaseSiswa.kt
@@ -1,6 +1,8 @@
 package com.example.room_database.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
@@ -8,5 +10,17 @@ import androidx.room.RoomDatabase
 abstract class DatabaseSiswa : RoomDatabase(){
     abstract fun siswaDao() : SiswaDao
 
+    companion object{
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
 
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(lock = this){
+                Room.databaseBuilder(
+                    context, klass = DatabaseSiswa::class.java,
+                    name = "siswa_database")
+                    .build().also { Instance=it }
+            })
+        }
+    }
 }
