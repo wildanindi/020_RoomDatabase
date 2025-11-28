dd03b5d6e5fd98847f4ea3a1b8493d15eb22a5e5
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-28 17:24:24 +0700

    Mendefinisikan objek `DestinasiEntry` yang mengimplementasikan `DestinasiNavigasi`

--- app/src/main/java/com/example/room_database/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/room_database/view/route/DestinasiEntry.kt
@@ -1,4 +1,8 @@
 package com.example.room_database.view.route
 
-object DestinasiEntry {
+import com.example.room_database.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.app_name
 }
