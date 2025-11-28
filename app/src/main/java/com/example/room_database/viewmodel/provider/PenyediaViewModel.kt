2a560ed479ebd38c31f1d2dc2b2227a6427a65e9
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-28 18:03:22 +0700

    Menyiapkan `Factory` PenyediaViewModel untuk inject `RepositoriSiswa` ke `HomeViewModel` dan `EntryViewModel`.

---
+++ app/src/main/java/com/example/room_database/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.room_database.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.room_database.viewmodel.HomeViewModel
+import com.example.room_database.repositori.AplikasiSiswa
+import com.example.room_database.viewmodel.EntryViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiSiswa)
