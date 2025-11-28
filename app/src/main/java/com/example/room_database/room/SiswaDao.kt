80de149cc85e50e0702ec2ecd8c7a74ded18c436
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-28 15:22:39 +0700

    Menambah File siswadao dan function untuk get all siswa

---
+++ app/src/main/java/com/example/room_database/room/SiswaDao.kt
@@ -0,0 +1,13 @@
+package com.example.room_database.room
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+
+}
