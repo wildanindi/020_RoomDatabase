a078b955875e27a59c83113b41171ef17177e97c
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-28 18:59:36 +0700

    memodifikasi MainActivity.kt agar aplikasi bisa di jalankan

--- app/src/main/java/com/example/room_database/MainActivity.kt
+++ app/src/main/java/com/example/room_database/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.room_database.ui.theme.Room_DatabaseTheme
+import com.example.room_database.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Room_DatabaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
