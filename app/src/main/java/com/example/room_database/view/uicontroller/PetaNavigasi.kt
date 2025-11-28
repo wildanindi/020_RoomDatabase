d2726c37a9cba749696c1d99eea890ec21c6f4af
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-28 18:58:46 +0700

    Setup `SiswaApp` dan `HostNavigasi`. Menghubungkan rute `HomeScreen` dan `EntrySiswaScreen`, termasuk fungsi navigasi maju dan mundur.

--- app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,44 @@
 package com.example.room_database.view.uicontroller
 
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.room_database.view.EntrySiswaScreen
+import com.example.room_database.view.HomeScreen
+import com.example.room_database.view.route.DestinasiHome
+import com.example.room_database.view.route.DestinasiEntry
+
+@Composable
+fun SiswaApp(modifier: Modifier = Modifier) {
+    val navController: NavHostController = rememberNavController()
+    HostNavigasi(navController = navController, modifier = modifier)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
