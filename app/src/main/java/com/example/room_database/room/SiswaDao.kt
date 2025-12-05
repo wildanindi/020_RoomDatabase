b780a6be11361dcbd8b8d6e598425061edf67b3e
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-05 14:42:49 +0700

    menambah query untuk ngambil data siswa (getSiswa), delete, dan update

--- app/src/main/java/com/example/room_database/room/SiswaDao.kt
+++ app/src/main/java/com/example/room_database/room/SiswaDao.kt
@@ -1,9 +1,11 @@
 package com.example.room_database.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,4 +15,13 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query(value = "SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
+    @Update
+    suspend fun update(siswa: Siswa)
 }
