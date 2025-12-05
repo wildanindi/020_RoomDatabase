a60e5da6af1519f6b307e0130a96513fea8d0f68
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-05 15:20:16 +0700

    Menambahkan navigasi halaman detail siswa dengan argumen ID

---
+++ app/src/main/java/com/example/room_database/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.room_database.view.route
+
+import com.example.room_database.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
