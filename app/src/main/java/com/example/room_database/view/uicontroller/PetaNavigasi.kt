0e13424c25b2ed8fae3b6852790206b96194f005
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-05 16:39:12 +0700

    Menambah menambah parameter yang di tambahkan pada halamanhome tadi agar navigasi berfungsi

--- app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
@@ -5,11 +5,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.room_database.view.DetailSiswaScreen
 import com.example.room_database.view.EntrySiswaScreen
 import com.example.room_database.view.HomeScreen
+import com.example.room_database.view.route.DestinasiDetailSiswa
+import com.example.room_database.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.room_database.view.route.DestinasiHome
 import com.example.room_database.view.route.DestinasiEntry
 
@@ -33,11 +38,26 @@ fun HostNavigasi(
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetailSiswa.route}/${it}")
+                }
             )
+
         }
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(
                 navigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(name = itemIdArg) {
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+
+                navigateBack = {navController.navigateUp()}
             )
         }
     }
