1776d571e1dec78d99207a4b0943f69112c27802
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-05 17:52:21 +0700

    Menambahkan route navigasi untuk halaman edit siswa

---
+++ app/src/main/java/com/example/room_database/view/route/DestinasiEditSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.room_database.view.route
+
+import com.example.room_database.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
